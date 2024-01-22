e11aae56c5c37c886f1c469915a1007d5264c865
Gaitano <gaitano.george@student.moringaschool.com>
2024-01-22 14:04:34 +0300

    Completed seeding the data

--- server/models.py
+++ server/models.py
@@ -17,7 +17,7 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String)
     last_name = db.Column(db.String)
-    full_name = db.Column(db.String)
+    _full_name = db.Column(db.String, name='full_name')
     username = db.Column(db.String)
     phone_no = db.Column(db.Integer, unique=True)
     profile = db.Column(db.String)
@@ -28,7 +28,7 @@ class User(db.Model, SerializerMixin):
     def __init__(self, first_name,last_name, username, phone_no, profile, about, email, password):
         self.first_name = first_name
         self.last_name = last_name
-        self.full_name = first_name + ' ' + last_name
+        self._full_name = f"{first_name} {last_name}" 
         self.username = username
         self.phone_no = phone_no
         self.profile = profile
@@ -41,6 +41,16 @@ class User(db.Model, SerializerMixin):
     
     def check_password(self, password):
         return bcrypt.check_password_hash(self.password, password)
+    
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    @full_name.setter
+    def full_name(self, value):
+        names = value.split(" ", 1)
+        self.first_name = names[0]
+        self.last_name = names[1] if len(names) > 1 else ""
 
 class Chat(db.Model, SerializerMixin):
     
@@ -51,7 +61,7 @@ class Chat(db.Model, SerializerMixin):
     sender = db.Column(db.Integer, db.ForeignKey('users.id'))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     
-    sender = db.relationship('User', backref='chats')
+    sender_user = db.relationship('User', backref='chats')
     
     def __init__(self, chat, sender):
         self.chat = chat
@@ -67,7 +77,8 @@ class Pair_chat(db.Model, SerializerMixin):
     receiver = db.Column(db.String, db.ForeignKey('users.id'))
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     
-    sender = db.relationship('User', backref = 'pair_chats')
+    sender_user = db.relationship('User', foreign_keys=[sender], backref = 'sent_pair_chats')
+    receiver_user = db.relationship('User', foreign_keys=[receiver], backref= 'received_pair_chats')
     
     def __init__(self, chat, sender, receiver):
         self.chat = chat
@@ -80,16 +91,17 @@ class Group(db.Model, SerializerMixin):
     
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    admin = db.Column(db.Integer, db.ForeignKey('users.id'))
+    admin_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     profile = db.Column(db.String)
     description = db.Column(db.String)
     
     admin = db.relationship('User', backref='groups')
     
-    def __init__(self, name, admin, profile):
+    def __init__(self, name, admin_id, profile, description):
         self.name = name
-        self.admin = admin
+        self.admin_id = admin_id
         self.profile = profile
+        self.description = description
 
 class Group_Member(db.Model, SerializerMixin):
     
@@ -113,12 +125,15 @@ class Group_Chat(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     group_id = db.Column(db.Integer, db.ForeignKey('groups.id'))
     sender = db.Column(db.Integer, db.ForeignKey('users.id'))
+    chat = db.Column(db.String)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     
-    group = db.relationship('Group', backref='group_members')
-    sender = db.relationship('User', backref='group_members')
+    group = db.relationship('Group', backref='group_chats')
+    sender_user = db.relationship('User', backref='group_chats_sender')
     
-    def __init__(self, group_id, sender):
+    def __init__(self, group_id, sender, chat):
         self.group_id = group_id
         self.sender = sender
+        self.chat = chat
+        
         
--- server/seed.py
+++ server/seed.py
@@ -23,16 +23,15 @@ with app.app_context():
     print('🦸‍♀️ Seeding users...')
     
     users = []
-    
-    for _ in range(100):
+    for _ in range(0, 100):
         user = User(
             first_name= fake.first_name(),
             last_name= fake.last_name(),
-            username = fake.username(),
+            username = fake.name(),
             phone_no= fake.phone_number(),
-            profile = f'https://dummyimage.com/200x200/{random.randint(10, 100000)}',
-            about = fake.text,
-            email= fake.email,
+            profile = fake.image_url(),
+            about = fake.text(),
+            email= fake.email(),
             password = 'helloworld'
         )
         users.append(user)
@@ -42,4 +41,72 @@ with app.app_context():
     
     print('🦸‍♀️ Seeding Chats')
     
+    chats=[]
+    for _ in range(1000):
+        chat = Chat(
+            chat = fake.text(),
+            sender= random.randint(1, 100)
+        )
+        chats.append(chat)
     
+    db.session.add_all(chats)
+    db.session.commit()
+    
+    
+    print('🦸‍♀️ Seeding pair_chats...')
+    
+    pair_chats = []
+    for _ in range(0, 900):
+        pair_chat = Pair_chat(
+            chat = fake.text(),
+            sender = random.randint(1, 100),
+            receiver = random.randint(1, 100)
+        )
+        pair_chats.append(pair_chat)
+    
+    db.session.add_all(pair_chats)
+    db.session.commit()
+
+    print('🦸‍♀️ Seeding group...')
+    
+    groups = []
+    for _ in range(0, 200):
+        group = Group(
+            name = fake.company(),
+            admin_id = random.randint(1, 100),
+            profile = fake.image_url(),
+            description = fake.text()
+        )
+        groups.append(group)
+        
+    db.session.add_all(groups)
+    db.session.commit()
+    
+    
+    print('🦸‍♀️ Seeding group_member...')
+    
+    group_members = []
+    for _ in range(0, 1000):
+        group_member = Group_Member(
+            group_id = random.randint(1, 200),
+            member_id = random.randint(1, 100)
+        )
+        group_members.append(group_member)
+    
+    db.session.add_all(group_members)
+    db.session.commit()
+    
+    print('🦸‍♀️ Seeding group_chats...')
+    
+    group_chats = []
+    
+    for _ in range(0, 10000):
+        group_chat = Group_Chat(
+            group_id  = random.randint(1, 200),
+            sender = random.randint(1, 100),
+            chat = fake.text()
+        )
+        group_chats.append(group_chat)
+        
+    db.session.add_all(group_chats)
+    db.session.commit()
